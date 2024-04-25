af220b11c55dfbacb0664b5ce128157ccf98a97f
aisng <tasignas@gmail.com>
2024-04-25 16:34:56 +0300

    before refac

--- main.go
+++ main.go
@@ -3,18 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"os"
 	"time"
-
-	"gopkg.in/yaml.v3"
 )
-
-// type Services struct {
-// 	Services []struct {
-// 		Url     string `yaml:"url"`
-// 		Timeout int    `yaml:"timeout"`
-// 	} `yaml:"services"`
-// }
 
 type Service struct {
 	Url     string `yaml:"url"`
@@ -27,50 +17,44 @@ type Config struct {
 }
 
 const defaultTimeout int = 3
+const defaultLimit int = 2
 const configPath string = "./config.yaml"
 
 func main() {
 
-	var servicesData Config
+	var downCount int
 
-	if _, err := os.Stat(configPath); err == nil {
+	config, err := getConfig(configPath)
 
-		fmt.Println("'config.yaml' found")
-		configData, err := parseConfigData(configPath)
-
-		if err != nil {
-			fmt.Println("Parse 'config.yaml' error:", err)
-		}
-
-		servicesData = *configData
-
-	} else {
-		fmt.Println("config.yaml not found. Generating with default values...")
-		configData, err := generateDefaultConfig()
-
-		if err != nil {
-			fmt.Println("Generate config.yaml error:", err)
-		}
-		servicesData = *configData
+	if err != nil {
+		fmt.Println(err)
 	}
 
-	for _, service := range servicesData.Services {
-		isRunning, err := pingService(service.Url, service.Timeout)
+	services := config.Services
 
+	for _, service := range services {
+		isRunning, err := requestHeadFromService(service.Url, service.Timeout)
+		// TODO: read service-status.json to check what was down before
 		if isRunning {
+			// TODO: send mail if there was sth down and is up now
 			fmt.Printf("Service %s is running\n", service.Url)
 		} else {
 			fmt.Printf("Service %s is down: '%s'\n", service.Url, err)
+			downCount += 1
+			// TODO: read service-status.json
 			// TODO: count how many down
-			// TODO: send email
+			// TODO: if more than limit, send email
+			// TODO: save in output.txt(?) which one is down
+			//
 		}
 		fmt.Println("-------------------------------------------")
-
 	}
+	fmt.Printf("Down count: %d\n", downCount)
+	fmt.Println(config.Limit > downCount)
 
 }
 
-func pingService(url string, timeout int) (bool, error) {
+func requestHeadFromService(url string, timeout int) (bool, error) {
 	client := http.Client{
 		Timeout: time.Second * time.Duration(timeout),
 	}
@@ -84,50 +68,4 @@ func pingService(url string, timeout int) (bool, error) {
 	defer resp.Body.Close()
 
 	return true, nil
-}
-
-func parseConfigData(path string) (*Config, error) {
-	data, err := os.ReadFile(path)
-
-	if err != nil {
-		fmt.Println(err)
-		return nil, err
-	}
-
-	var config Config
-
-	err = yaml.Unmarshal(data, &config)
-	fmt.Printf("servicefromparser: %v\n", config)
-	if err != nil {
-		fmt.Println(err)
-		return nil, err
-	}
-	return &config, nil
-}
-
-func generateDefaultConfig() (*Config, error) {
-	defaultConfig := Config{
-		Services: []Service{
-			{Url: "http://10.162.222.151/", Timeout: defaultTimeout},
-			{Url: "https://prod.alm.gpdm.fresenius.com", Timeout: defaultTimeout},
-			{Url: "http://desw-lizenz.schweinfurt.germany.fresenius.de", Timeout: defaultTimeout},
-			{Url: "https://central.artifactory.alm.gpdm.fresenius.com", Timeout: defaultTimeout},
-			{Url: "https://qdok.ads.fresenius.com/", Timeout: defaultTimeout},
-			{Url: "https://www.lrytas.lt", Timeout: defaultTimeout},
-		},
-		Limit: 2,
-	}
-
-	yamlData, err := yaml.Marshal(&defaultConfig)
-
-	if err != nil {
-		return nil, err
-	}
-
-	// 0644 permission to read, write
-	if err = os.WriteFile("config.yaml", yamlData, 0644); err != nil {
-		return nil, err
-	}
-	return &defaultConfig, nil
-
 }

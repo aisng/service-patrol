51c45201a92093954733112d252d2169a424fedd
aisng <tasignas@gmail.com>
2024-05-06 16:44:13 +0300

    rm service struct fields

--- config.go
+++ config.go
@@ -38,13 +38,13 @@ func (c *Config) GenerateDefault() {
 // 		DownLimit: defaultLimit,
 // 		Timeout:   defaultTimeout,
 // 		Frequency: defaultFrequency,
-// 		Services: []Service{
-// 			{Url: "http://10.162.222.151"},
-// 			{Url: "https://prod.alm.gpdm.fresenius.com"},
-// 			{Url: "http://desw-lizenz.schweinfurt.germany.fresenius.de"},
-// 			{Url: "https://central.artifactory.alm.gpdm.fresenius.com"},
-// 			{Url: "https://qdok.ads.fresenius.com"},
-// 			{Url: "https://www.lrytas.lt"},
+// 		Services: []string{
+// 			"http://10.162.222.151",
+// 			"https://prod.alm.gpdm.fresenius.com",
+// 			"http://desw-lizenz.schweinfurt.germany.fresenius.de",
+// 			"https://central.artifactory.alm.gpdm.fresenius.com",
+// 			"https://qdok.ads.fresenius.com",
+// 			"https://www.lrytas.lt",
 // 		},
 // 		MailingList: []string{
 // 			"mohammad.abshir@dockerbike.com",

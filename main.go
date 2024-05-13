cb79cda1c511918fbca4ce6850d4abf274233ba8
aisng <tasignas@gmail.com>
2024-05-13 16:36:17 +0300

    simplify main.go

--- main.go
+++ main.go
@@ -10,7 +10,6 @@ func main() {
 
 	if err := config.Read(); err != nil {
 		panic(err)
-		// return
 	}
 
 	if err := serviceStatus.Read(); err != nil {
@@ -18,24 +17,21 @@ func main() {
 		return
 	}
 
-	servicePatrol := NewServicePatrol(
-		&config,
-		&serviceStatus,
-	)
+	sp := NewServicePatrol(&config, &serviceStatus)
 
-	down, recovered := servicePatrol.Start()
+	down, recovered := sp.Start()
 
 	if down != nil || recovered != nil {
 		// TODO: figure out "chained" ptrs/deref
-		msg := NewMessage(servicePatrol.DownServices, servicePatrol.RecoveredServices, config.Frequency)
+		msg := NewMessage(down, recovered, config.Frequency)
 		msgStr, err := ParseTemplate(msg, messageTemplate)
 		if err != nil {
 			fmt.Println(err)
 		}
-		// err = SendMail(config.MailingList, msgStr)
-		// if err != nil {
-		// 	fmt.Println(err)
-		// }
+		err = SendMail(config.MailingList, msgStr)
+		if err != nil {
+			fmt.Println(err)
+		}
 
 		fmt.Printf("down_count (%d) >= down_limit (%d). Email sent.\n", serviceStatus.DownCount, config.DownLimit)
 

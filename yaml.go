bc65cda1a528e18ec8dd7a96f7e536e069f39f1c
aisng <tasignas@gmail.com>
2024-05-13 16:26:18 +0300

    rm commented code

--- yaml.go
+++ yaml.go
@@ -8,9 +8,6 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
-// TODO: reconsider the necessity of parametrizing interface methods
-// when default values can be passed straight to writeYaml and readYaml
-
 type YamlData interface {
 }
 
@@ -40,38 +37,3 @@ func readYaml(filename string, yd YamlData) error {
 	}
 	return nil
 }
-
-// func validateFields(filename string, yd YamlData) error {
-// 	structType := reflect.TypeOf(yd).Elem()
-// 	structFieldCount := structType.NumField()
-
-// 	data := make(map[string]any, structFieldCount)
-
-// 	if err := readYaml(filename, &data); err != nil {
-// 		return err
-// 	}
-
-// 	// var missingKeys []string
-
-// 	for i := 0; i < structFieldCount; i++ {
-// 		field := structType.Field(i)
-// 		yamlTag := field.Tag.Get("yaml")
-
-// 		_, key := data[yamlTag]
-
-// 		if !key {
-// 			// missingKeys = append(missingKeys, yamlTag)
-// 			return fmt.Errorf("missing field in '%s': '%s'", filename, yamlTag)
-// 		}
-// 	}
-
-// 	// if len(missingKeys) > 0 {
-// 	// 	return fmt.Errorf("missing fields in %s:\n%s", filename, strings.Join(missingKeys, "\n"))
-// 	// }
-
-// 	if err := readYaml(filename, yd); err != nil {
-// 		return err
-// 	}
-
-// 	return nil
-// }
